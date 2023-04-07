68bd0cef8b3be3f8d8c694c19629bf85cd930b97
bismaarif <bismaarif1616@gmail.com>
2023-04-07 18:10:48 +0500

    javascript task 1 complete

---
+++ app.js
@@ -0,0 +1,20 @@
+//alert("javascript task#1");
+
+
+// define the zakat percentage
+var zakatPercentage = 0.025;
+
+
+// Take input from the user the converting it to a number
+var userinput = +prompt("Enter your total value of zakat","zakat value must be a number:");
+
+
+// calculate the zakat amount
+var result = zakatPercentage * userinput;
+
+
+// Display the zakat amount to the user
+alert("your zakat value is " + result);
+
+
+
